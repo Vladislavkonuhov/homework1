ff4cfa841cf43948e37c3c86b09259581161fe82
Name <vkonohov19@gmail.com>
2023-05-26 15:16:40 +0300

    dz1

---
+++ Task1/dz1.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int n1 = int.Parse(Console.ReadLine()!);
+if (n < n1)
+    Console.WriteLine(n1);
+else
+    Console.Write(n);
