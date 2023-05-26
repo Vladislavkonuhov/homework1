7f5bc2b6b78fbd64968c1f9bae8a89ef9bdc190e
Name <vkonohov19@gmail.com>
2023-05-26 15:46:05 +0300

    dz4

---
+++ Task4/dz4.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int n1 = 2;
+if (n > 1)
+    while(n1 <= n)
+    {
+         Console.Write(n1 + " ");
+         n1 = n1 + 2;
+    }
