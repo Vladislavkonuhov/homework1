5d2cbed8d8de6143ce028a79c826dc285bcd83d9
Name <vkonohov19@gmail.com>
2023-05-26 15:23:50 +0300

    dz2

---
+++ Task2/dz2.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int n1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int n2 = int.Parse(Console.ReadLine()!);
+int max = n;
+if (n<n1)
+    max = n1;
+if (n1<n2)
+    max = n2;
+Console.WriteLine(max);
